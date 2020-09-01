f0e25a0418a30bf5f8a012e9d45fa43aa91a0081
榎本拓真 <t3460515@gmail.com>
2020-09-01 23:29:55 +0900

    [Finish]ouyou2

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def ensure_correct_user
+    @book = Book.find_by(id:params[:id])
+    if @book.user_id != current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
+end
